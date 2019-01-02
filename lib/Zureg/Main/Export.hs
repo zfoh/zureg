68cce3b742aafd7c3928b64ec798103fe9f8ff4a
Charles Till <charles@asimovmed.com>
2019-01-02 18:46:30 +0100

    Add CSV export

--- lib/Zureg/Main/Export.hs
+++ lib/Zureg/Main/Export.hs
@@ -9,9 +9,11 @@ import qualified Data.ByteString.Lazy.Char8 as BL
 import           System.Directory           (doesFileExist)
 import           System.Environment         (getArgs, getProgName)
 import           System.Exit                (exitFailure)
+import qualified Data.Csv                   as CSV
 import qualified System.IO                  as IO
 import qualified Zureg.Config               as Config
 import qualified Zureg.Database             as Database
+import qualified Zureg.Model                as Model
 
 progressMapM :: (a -> IO b) -> [a] -> IO [b]
 progressMapM f xs = forM (zip [1 :: Int ..] xs) $ \(n, x) -> do
@@ -31,17 +33,26 @@ main = do
     dbConfig <- Config.section config "database"
 
     case args of
-        [path] -> do
+        [path, format] -> do
             exists <- doesFileExist path
             when exists $ fail $ path ++ " already exists"
-            Database.withHandle dbConfig $ \db -> do
-                uuids       <- Database.getRegistrantUuids db
-                registrants <- progressMapM (Database.getRegistrant db) uuids
-                BL.writeFile path $ A.encode registrants
+            case format of
+                "json" -> Database.withHandle dbConfig $ \db -> do
+                        uuids       <- Database.getRegistrantUuids db
+                        registrants <- progressMapM (Database.getRegistrant db) uuids
+                        BL.writeFile path $ A.encode registrants
+                "csv" -> Database.withHandle dbConfig $ \db -> do
+                        uuids       <- Database.getRegistrantUuids db
+                        registrants <- progressMapM (Database.getRegistrant db) uuids
+                        BL.writeFile path $ CSV.encodeByName Model.itemHeader registrants
+                _   -> do 
+                    IO.hPutStr IO.stderr $ unlines
+                        [ "Unkown format: " ++ format ]
+                    exitFailure   
 
         _ -> do
             IO.hPutStr IO.stderr $ unlines
-                [ "Usage: " ++ progName ++ " export.json"
+                [ "Usage: " ++ progName ++ " <filename> <format>"
                 , ""
                 , "Export registrants from the database into a JSON file"
                 ]
