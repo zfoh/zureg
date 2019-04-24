3a7528211a6812d51284a008e1b2c52eda4b066b
Charles Till <charles.till@gmail.com>
2019-04-24 18:38:17 +0200

    Simplify confirm handler, second attempt

--- lib/Zureg/Main/Lambda.hs
+++ lib/Zureg/Main/Lambda.hs
@@ -108,11 +108,9 @@ main = do
                 uuid <- getUuidParam req
                 registrant <- Database.getRegistrant db uuid
                 case rState registrant of 
-                  Just Registered -> do Database.writeEvents db uuid [Confirm]
-                                        return $ Serverless.response302 $
-                                          "ticket?uuid=" <> E.uuidToText uuid
-                  _               -> return $ Serverless.response302 $
-                                          "ticket?uuid=" <> E.uuidToText uuid
+                  Just Registered -> Database.writeEvents db uuid [Confirm]
+                  _               -> return ()
+                return $ Serverless.response302 $ "ticket?uuid=" <> E.uuidToText uuid
                    
             ["cancel"] -> do
                 (view, mbCancel) <- Serverless.runForm req "cancel" $
