994698d921c270342d7144632cd3ea52f1e9da36
Jasper Van der Jeugt <m@jaspervdj.be>
2019-04-29 12:07:11 +0200

    Allow passing multiple UUIDs to zureg-pop-waitlist

--- lib/Zureg/Main/PopWaitlist.hs
+++ lib/Zureg/Main/PopWaitlist.hs
@@ -3,6 +3,7 @@ module Zureg.Main.PopWaitlist
     ( main
     ) where
 
+import           Control.Monad             (forM, when, forM_)
 import qualified Data.Text                 as T
 import qualified Data.Time                 as Time
 import qualified Eventful                  as E
@@ -24,32 +25,33 @@ main = do
     dbConfig    <- Config.section config "database"
     emailConfig <- Config.section config "sendEmail"
 
-    case args of
-        [uuidstr] ->
-            Database.withHandle dbConfig $ \db ->
-            SendEmail.withHandle emailConfig $ \mailer -> do
-                uuid <- maybe (fail "could not parse uuid") return
-                    (E.uuidFromText $ T.pack uuidstr)
-                registrant <- Database.getRegistrant db uuid
-                event <- PopWaitlist . PopWaitlistInfo <$> Time.getCurrentTime
-                let registrant' = E.projectionEventHandler
-                        (registrantProjection uuid) registrant event
+    uuids <- forM args $
+        maybe (fail "could not parse uuid") return .  E.uuidFromText . T.pack
 
-                -- Sanity checks
-                rinfo <- case rInfo registrant' of
-                    Nothing                       -> fail "missing info?"
-                    _ | registrant == registrant' -> fail "not waitlisted?"
-                    Just i                        -> return i
+    when (null args) $ do
+        IO.hPutStr IO.stderr $ unlines
+            [ "Usage: " ++ progName ++ " [uuids...]"
+            , ""
+            , "Pop registrants from the waiting list"
+            ]
+        exitFailure
 
-                IO.hPutStrLn IO.stderr "Writing event..."
-                Database.writeEvents db uuid [event]
-                IO.hPutStrLn IO.stderr "Sending email..."
-                sendPopWaitlistEmail mailer rinfo uuid
-                IO.hPutStrLn IO.stderr "OK"
-        _ -> do
-            IO.hPutStr IO.stderr $ unlines
-                [ "Usage: " ++ progName ++ " <uuid>"
-                , ""
-                , "Pop a registrant from the waiting list"
-                ]
-            exitFailure
+    Database.withHandle dbConfig $ \db ->
+        SendEmail.withHandle emailConfig $ \mailer ->
+        forM_ uuids $ \uuid -> do
+            registrant <- Database.getRegistrant db uuid
+            event <- PopWaitlist . PopWaitlistInfo <$> Time.getCurrentTime
+            let registrant' = E.projectionEventHandler
+                    (registrantProjection uuid) registrant event
+
+            -- Sanity checks
+            rinfo <- case rInfo registrant' of
+                Nothing                       -> fail "missing info?"
+                _ | registrant == registrant' -> fail "not waitlisted?"
+                Just i                        -> return i
+
+            IO.hPutStrLn IO.stderr "Writing event..."
+            Database.writeEvents db uuid [event]
+            IO.hPutStrLn IO.stderr "Sending email..."
+            sendPopWaitlistEmail mailer rinfo uuid
+            IO.hPutStrLn IO.stderr "OK"
