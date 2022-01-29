85dbd2f3931a563296fabdc178329c994172d5b1
Jasper Van der Jeugt <m@jaspervdj.be>
2022-01-29 16:28:58 +0100

    Add some code for ZuriHac 2022

---
+++ lib/Zureg/Hackathon/ZuriHac2022.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Zureg.Hackathon.ZuriHac2022
+    ( newHackathon
+    ) where
+
+import qualified Data.Text                           as T
+import           System.Environment                  (getEnv)
+import qualified Text.Blaze.Html5                    as H
+import qualified Zureg.Database                      as Database
+import           Zureg.Hackathon.Interface           (Hackathon)
+import qualified Zureg.Hackathon.Interface           as Hackathon
+import           Zureg.Hackathon.ZuriHac2020.Discord as Discord
+import           Zureg.Hackathon.ZuriHac2022.Form    as ZH22
+import           Zureg.Hackathon.ZuriHac2022.Model   as ZH22
+import qualified Zureg.ReCaptcha                     as ReCaptcha
+import qualified Zureg.SendEmail                     as SendEmail
+
+newHackathon :: IO (Hackathon RegisterInfo)
+newHackathon = do
+    scannerSecret   <- T.pack <$> getEnv "ZUREG_SCANNER_SECRET"
+    reCaptchaSecret <- T.pack <$> getEnv "ZUREG_RECAPTCHA_SECRET"
+    email           <- T.pack <$> getEnv "ZUREG_EMAIL"
+
+    discord <- Discord.configFromEnv
+    channel <- Discord.getWelcomeChannelId discord
+
+    return Hackathon.Hackathon
+        { Hackathon.name = "ZuriHac 2022"
+        , Hackathon.baseUrl = "https://zureg.zfoh.ch"
+        , Hackathon.contactUrl = "https://zfoh.ch/zurihac2022/#contact"
+        , Hackathon.capacity = 3000
+        , Hackathon.confirmation = False
+
+        , Hackathon.registerBadgeName = False
+        , Hackathon.registerAffiliation = False
+
+        , Hackathon.registerForm = ZH22.additionalInfoForm
+        , Hackathon.registerView = ZH22.additionalInfoView
+        , Hackathon.ticketView = mempty
+        , Hackathon.scanView = mempty
+        , Hackathon.csvHeader = ZH22.csvHeader
+
+        , Hackathon.databaseConfig = Database.defaultConfig
+        , Hackathon.sendEmailConfig = SendEmail.Config
+            { SendEmail.cFrom = "ZuriHac Registration Bot <" <> email <> ">"
+            }
+        , Hackathon.reCaptchaConfig = ReCaptcha.Config
+            { ReCaptcha.cEnabled   = True
+            , ReCaptcha.cSiteKey   = "6LcVUm8UAAAAAL0ooPLkNT3O9oEXhGPK6kZ-hQk7"
+            , ReCaptcha.cSecretKey = reCaptchaSecret
+            }
+        , Hackathon.scannerSecret = scannerSecret
+        , Hackathon.chatUrl = Discord.generateTempInviteUrl discord channel
+        , Hackathon.chatExplanation = H.p $ do
+            "Even though it will hopefully take place as a physical event, "
+            "ZuriHac 2022 will use Discord as a chat platform for "
+            "coordination.  You can join the Discord server here:"
+        }
---
+++ lib/Zureg/Hackathon/ZuriHac2022/Form.hs
@@ -0,0 +1,97 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Zureg.Hackathon.ZuriHac2022.Form
+    ( additionalInfoForm
+    , additionalInfoView
+    ) where
+
+import qualified Data.Text                         as T
+import qualified Text.Blaze.Html5                  as H
+import qualified Text.Blaze.Html5.Attributes       as A
+import qualified Text.Digestive                    as D
+import qualified Text.Digestive.Blaze.Html5        as DH
+import           Zureg.Hackathon.ZuriHac2022.Model as ZH22
+
+additionalInfoForm :: Monad m => D.Form H.Html m ZH22.RegisterInfo
+additionalInfoForm = RegisterInfo
+    <$> ("tshirt" D..: (D.validate tshirtCheck $ (,)
+            <$> "cut" D..: D.choice
+                    [ (Nothing,     "I don't want a T-Shirt")
+                    , (Just Female, "Female")
+                    , (Just Male,   "Male")
+                    ] (Just (Just Male))
+            <*> "size" D..: D.choice (
+                    [(Just s, H.toHtml $ show s) | s <- [minBound .. maxBound]] ++
+                    [(Nothing, "I don't want a T-Shirt")])
+                    (Just (Just M))))
+    <*> "region" D..: D.choice (
+            (Nothing, "I'd rather not say") :
+            [(Just s, H.toHtml $ show s) | s <- [minBound .. maxBound]])
+            (Just Nothing)
+    <*> "occupation" D..: D.choice
+            [ (Nothing,       "I'd rather not say")
+            , (Just Student,  "I am a student")
+            , (Just Tech,     "I work in the tech sector")
+            , (Just Academia, "I work in academia")
+            , (Just Other,    "Other")
+            ]
+            (Just Nothing)
+    <*> ("project" D..: (Project
+            <$> "name" D..: optionalText
+            <*> "website" D..: optionalText
+            <*> "description" D..: optionalText
+            <*> ("contributorLevel" D..: (ContributorLevel
+                    <$> "beginner" D..: D.bool Nothing
+                    <*> "intermediate" D..: D.bool Nothing
+                    <*> "advanced" D..: D.bool Nothing))))
+  where
+    tshirtCheck (Just c,  Just s)  = D.Success . Just $ TShirtInfo c s
+    tshirtCheck (Nothing, Nothing) = D.Success Nothing
+    tshirtCheck (_,       _)       = D.Error
+        "Fill in both T-Shirt cut and size or neither of the two"
+
+    optionalText =
+        (\t -> let t' = T.strip t in if T.null t' then Nothing else Just t') <$>
+        (D.text Nothing)
+
+additionalInfoView :: D.View H.Html -> H.Html
+additionalInfoView view = do
+    H.h2 "Optional information"
+
+    H.p $ H.strong "Region"
+    DH.label "region" view $ do
+        "From what area will you attend ZuriHac?  This is purely for our "
+        "statistics."
+    DH.inputSelect "region" view
+    H.br
+
+    H.p $ H.strong "Occupation"
+    DH.label "region" view $
+        "What is your occupation?  This is purely for our statistics."
+    DH.inputSelect "region" view
+    H.br
+
+    H.h2 "Project (optional)"
+    H.p $ do
+        "Do you have a project or an idea to hack on with others? Do you have "
+        "something you want to teach people?"
+    H.p $ do
+        "We greatly appreciate projects. We have had very good experience with "
+        "announcing the project early on the homepage, so that potential "
+        "participants can prepare before the Hackathon.  Of course, we're also "
+        "happy to add projects during the Hackathon itself, so if you're not "
+        "sure yet, don't worry about it."
+    DH.label "project.name" view "Project name"
+    DH.inputText "project.name" view
+    DH.label "project.website" view "Project website"
+    DH.inputText "project.website" view
+    DH.label "project.description" view "Project description"
+    DH.inputText "project.description" view
+    H.p "Recommended contributor level(s)"
+    DH.inputCheckbox "project.contributorLevel.beginner" view H.! A.class_ "checkbox"
+    DH.label "project.contributorLevel.beginner" view $ "Beginner"
+    H.br
+    DH.inputCheckbox "project.contributorLevel.intermediate" view H.! A.class_ "checkbox"
+    DH.label "project.contributorLevel.intermediate" view $ "Intermediate"
+    H.br
+    DH.inputCheckbox "project.contributorLevel.advanced" view H.! A.class_ "checkbox"
+    DH.label "project.contributorLevel.advanced" view $ "Advanced"
